786a6f650204905ec2d67484a8cb24042d06ffc7
SeanProfeta <sprofeta001@student.butte.edu>
2017-08-23 18:56:35 +0000

    Lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,13 @@
+Player will click a button to begin game
+The computer will output two random numbers 
+The computer will explain to the player they must guess the correct number between these two numbers
+The computer will also display a ten on the top right of the screen
+The player will then have a small box to type in which they can guess
+The player will then pick the middle number between these two numbers 
+If the player is correct the computer will display a winning message. If not contuine on 
+The computer will then say higher/lower if the player is incorrect 
+The number will in the top right will then decrease by one from whatever number is displayed there.
+Step tep 7-9 will then contuine to loop untill either the player wins the game or the number in the top right corner turns to zero 
+After the number displays 0 a loser message will pop on the screen 
+Regradeless if you win or lose the screen will also display a "Try again?" message 
+If the player choses the do so steps 1-12 will repeat untill the player no longer clicks "Try again?"
