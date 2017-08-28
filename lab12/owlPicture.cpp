152edc2a142c678174de66832ec7390243c481de
SeanProfeta <sprofeta001@student.butte.edu>
2017-08-28 19:16:46 +0000

    lab1.2commit

---
+++ lab12/owlPicture.cpp
@@ -0,0 +1,13 @@
+// Created by Sean Profeta
+// Date: 8/28/17
+#include <iostream>
+using namespace std;
+int main ()
+{
+std::cout <<" \\" << "," << "_______" << "," << "/" << std::endl;
+cout << "   '" << "(" << "@" << "'" << "@"  << ")" << "'" <<endl;
+cout << "((" << "lllllllll" << ")" << ")" << ")" << endl;
+cout << "((" << "lllllllll" << ")" << ")" << ")" << endl;
+cout << "   \"" <<"     \"" << endl;
+cout << "_ _" << "\""<< "_ _ _" << "\""<< " _ _" << "," << "'" << "-" << endl;
+}
