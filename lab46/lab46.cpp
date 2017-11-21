ebe92391127945a5d8c1b413881a83f45f758527
SeanProfeta <sprofeta001@student.butte.edu>
2017-11-21 07:39:08 +0000

    input

---
+++ lab46/lab46.cpp
@@ -0,0 +1,76 @@
+//Created by : Sean Profeta
+//date Created : 11/20/17
+//The point of this program is too take information from a file and output it. THe program will take a students information such as amount of classes taken and their grades in it to calclate their gpa and out put it into and html page
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <iomanip>
+#include <ios>
+#include <string>
+using namespace std;
+int main (){
+    //THis is where I put most of the variables i add a few later
+    string fname;
+    string lname;
+    int numcourses =0;
+    int hours [10]={0};
+    char grade [10];
+    double points[10]={0.0};
+    int totalhours =0;
+    double totalpoints=0.0;
+    //This section is used to open the files 
+    ifstream fin;
+    ofstream fout;
+    fin.open("input.txt");
+    fout.open("output.txt");
+    // This while function will work while the file is open 
+    while(!fin.eof()){
+        fin >> fname;
+        fin >> lname;
+        fin >> numcourses;
+    
+    for (int i=0; i < numcourses; i++){
+        fin >> hours[i];
+        fin >> grade[i];
+    
+// This is where the letter grades are calculated to the gpa.   
+if(grade[i] == 'A') {
+        points[i] =4.0;
+    }
+    else if (grade[i] == 'B'){
+        points[i]=3.0;
+    }
+    else if (grade[i] == 'C'){
+        points[i] = 2.0;
+    }
+    else if (grade[i] == 'D'){
+        points [i] = 1.0;
+    }
+    else if (grade[i]== 'F'){
+        points[i]=0.0;
+    }
+}
+for( int i=0; i<numcourses;i++){
+    totalhours = totalhours + hours[i];
+    totalpoints=totalpoints+(hours[i]*points[i]);
+    //Calculating the gpa
+}
+double gpa= totalpoints/totalhours;
+fout << "Name: " << fname << " " << lname << endl;
+fout <<"Number of courses enrolled in " << numcourses << endl;
+fout <<"Hours Grade" << endl;
+
+for (int i=0;i<numcourses;i++){
+    fout<<setw(5) << hours[i] << " " << setw(5) << grade[i] << endl ;
+}
+//set the gpa to be two decimals instead of a unless number
+fout << "GPA: " << fixed << setprecision(2) << gpa << endl;
+totalhours=0;
+totalpoints=0.0;
+}
+//use to close the file
+fin.close();
+fout.close(); 
+return 0;
+
+}
