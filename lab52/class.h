ae19dd423f5b7dff82dd383548f2ea2907887681
SeanProfeta <sprofeta001@student.butte.edu>
2017-11-30 05:30:04 +0000

    class.cpp

---
+++ lab52/class.h
@@ -0,0 +1,17 @@
+#include "header.h"
+class MyClass {
+public:
+ /*
+ * Constructor
+ * Initialize our member variable num_ to 0
+ */
+ MyClass();
+ /*
+ * Output our member variable using cout
+ */
+ void Output();
+  void SetNumber(int num);
+  int GetNumber();
+private:
+ int num_;
+};
---
+++ lab52/main.cpp
@@ -0,0 +1,23 @@
+#include "header.h"
+#include "class.h"
+#include "main.cpp"
+
+int randomNumber(){
+ srand(time(0));
+ return (rand()% 10) + 1);
+}
+
+// Program starts here
+int main() {
+ // Create a MyClass Object
+ MyClass object;
+
+  //Send a number to the Object
+  object.SetNumber(randomNumber());
+
+ // Call the output member function
+ object.Output();
+
+ // This ends our program
+ return 0;
+}
